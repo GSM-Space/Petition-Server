76cf474b78bd9050d99b0916a7c6769b68792fe8
sunrabbit123 <qudwls185@naver.com>
2020-10-27 21:26:33 +0900

    add PORT setting

--- server.py
+++ server.py
@@ -10,8 +10,10 @@ PORT = config.get("defalut", "PORT") | 5000
 
 reload = False
 if len(sys.argv) >= 2:
-    if sys.argv[1] == "-R":
+    if "-R" in sys.argv[1]:
         reload = True
+    if "PORT=" in sys.argv[-1]:
+        PORT = sys.argv[-1][5:]
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port = PORT, reload = reload)
