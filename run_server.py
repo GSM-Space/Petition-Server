9195de9f51a309f40811b82aaeb61d2d554faa8b
sunrabbit123 <qudwls185@naver.com>
2020-11-02 16:44:32 +0900

    move dir

--- run_server.py
+++ run_server.py
@@ -1,7 +1,7 @@
 import uvicorn
 import sys, getopt
 
-from app.app import app
+from src.app import app
 from configparser import ConfigParser
 
 
@@ -10,10 +10,17 @@ def main(argv : list() = sys.argv):
     """
     reload 옵션과 PORT를 지정해주는 함수
     """
+
     config = ConfigParser()
+    config.read('config.ini')
+
     FILE_NAME = argv[0]
     reload = False
-    PORT = config.get("defalut", "PORT") | 5000
+    try:
+        PORT : int = int(config.get("default", "PORT"))
+    except Exception as e:
+        print(e)
+        PORT = 5000
 
     try:
         opts, etc_args = getopt.getopt(argv[1:],\
@@ -35,7 +42,7 @@ def main(argv : list() = sys.argv):
             PORT =  arg
             # "PORT setting option : -P args, --PORT=args"
 
-    uvicorn.run("main:app", port = PORT, reload = reload)
+    uvicorn.run("src.app:app", port = PORT, reload = reload)
 
 if __name__ == "__main__":
     main(sys.argv)
