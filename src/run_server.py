77717fc4d56f8f2fe3453b9198847a6f069ce83a
hmoon630 <s19017@gsm.hs.kr>
2020-11-03 22:50:12 +0900

    move run_server to src/

--- src/run_server.py
+++ src/run_server.py
@@ -1,52 +1,46 @@
 import uvicorn
 import sys, getopt
 
-from src.app import app
+from app import app
 from configparser import ConfigParser
 
 
-
-def main(argv : list() = sys.argv):
+def main(argv: list() = sys.argv):
     """
     reload 옵션과 PORT를 지정해주는 함수
     """
 
     config = ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
 
     FILE_NAME = argv[0]
     reload = False
     try:
-        PORT : int = int(config.get("default", "PORT"))
+        PORT: int = int(config.get("default", "PORT"))
     except Exception as e:
         print(e)
         PORT = 5000
 
     try:
-        opts, etc_args = getopt.getopt(argv[1:],\
-                                        "HRP:",\
-                                        ["reload", "PORT="])
+        opts, etc_args = getopt.getopt(argv[1:], "HRP:", ["reload", "PORT="])
 
     except getopt.GetoptError:
-        print(FILE_NAME, '-R -P <Port Number>')
+        print(FILE_NAME, "-R -P <Port Number>")
         sys.exit(2)
-    
+
     for opt, arg in opts:
         if opt in ("-H", "--help"):
-            print(FILE_NAME, '-R -P <Port Number>')
+            print(FILE_NAME, "-R -P <Port Number>")
             sys.exit(2)
         elif opt in ("-R", "--reload"):
             reload = True
-             # "reload setting option : -R, --reload"
+            # "reload setting option : -R, --reload"
         elif opt in ("-P", "--PORT"):
-            PORT =  arg
+            PORT = arg
             # "PORT setting option : -P args, --PORT=args"
 
-    uvicorn.run("src.app:app", port = PORT, reload = reload)
+    uvicorn.run("app:app", port=PORT, reload=reload)
+
 
 if __name__ == "__main__":
     main(sys.argv)
-
-
-
-
