a063473ac91a163676af16e271bfe8d9770c4845
hmoon630 <s19017@gsm.hs.kr>
2020-11-11 11:02:44 +0900

    [MISC] line endings

--- src/model/Response/user.py
+++ src/model/Response/user.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel
 from typing import List
+
+
 class Account:
     """
     name은 유저 닉네임
     consent는 청원 동의한 것들의 리스트
     """
-    name : str
-    consent : List[str]
+
+    name: str
+    consent: List[str]
