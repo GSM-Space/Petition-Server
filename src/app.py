90adfdbab420bd500530330e58fffdd6dce51fb8
hmoon630 <s19017@gsm.hs.kr>
2020-11-11 09:39:29 +0900

    add DB connector middleware

--- src/app.py
+++ src/app.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+import configparser
 
+from middlewares.DBSession import DBSession
 from routers import test
 
-app = FastAPI()
+config = configparser.ConfigParser()
+config.read("config.ini")
 
+app = FastAPI()
+app.add_middleware(DBSession, db_url=config.get("default", "DB_URL"))
 
 app.include_router(
     test.router,
     prefix="/api",
     tags=["api"],
-    responses={404: {"description" : "Not found"}},
+    responses={404: {"description": "Not found"}},
 )
---
+++ src/middlewares/DBSession.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from fastapi_sqlalchemy import DBSessionMiddleware
+from model.Database import *
+
+
+class DBSession(DBSessionMiddleware):
+    def __init__(self, db_url=None, custom_engine=None, *args, **kwargs):
+        if not db_url and not custom_engine:
+            raise ValueError("You need to pass a db_url or a custom_engine parameter.")
+
+        if not custom_engine:
+            custom_engine = create_engine(db_url)
+
+        Base.metadata.create_all(custom_engine)
+        super().__init__(custom_engine=custom_engine, *args, **kwargs)
