e75b58d592a7c43110f3de536710c0dd25d210b4
sunrabbit123 <qudwls185@naver.com>
2020-11-30 17:30:16 +0900

    ADD create petition path

--- src/routers/api.py
+++ src/routers/api.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from sqlalchemy.orm import Session
+
+from fastapi import APIRouter, Depends
+from model.Response.petition import CreatePetition
+from model.Database.petitions import create_petition as new_petition
 
 router = APIRouter()
 
-@router.get("/test")
-async def test():
-    return {"name" : "sunrabbit123"}
+@router.post("/test")
+def test():
+    return "petition"
 
-@router.post("/create")
-async def create_petition():
-    
+@router.post("/create/petition")
+def create_petition(req_form : CreatePetition):
+    #TODO 사용자의 입력값 검증
+    return new_petition(req_form)
