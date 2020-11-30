96adec3e6e8c7287d4e1d5deb786c9a2c2507172
sunrabbit123 <qudwls185@naver.com>
2020-11-30 17:29:17 +0900

    MISC change import from root

--- src/middlewares/DBSession.py
+++ src/middlewares/DBSession.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from fastapi_sqlalchemy import DBSessionMiddleware
-from model import Base
 
+from model.Database import Base
 
 class DBSession(DBSessionMiddleware):
     def __init__(self, db_url=None, custom_engine=None, *args, **kwargs):
         if not db_url and not custom_engine:
             raise ValueError("You need to pass a db_url or a custom_engine parameter.")
-
         if not custom_engine:
             custom_engine = create_engine(db_url)
 
