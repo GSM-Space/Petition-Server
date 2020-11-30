bd4141cc8fc18b2042114783242e31cc8a639ade
sunrabbit123 <qudwls185@naver.com>
2020-11-30 17:30:51 +0900

    Update class abstraction

--- src/model/Response/petition.py
+++ src/model/Response/petition.py
@@ -2,34 +2,29 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import List
 
-
 class Petition(BaseModel):
 
-    petitioner : str
-    title: str
+    title : str
+
+class CreatePetition(Petition):
+
+    petitioner : int
     contents: str
     proposal : str
-    agreed : int
-    answer : str
-    status : int = 1
-    created_at: datetime = datetime.now()
-
-
-class Total_Petition(BaseModel):
-    """
-    총 청원에 관한 모델
-    Petition은 청원들의 리스트
-    expiration_Petition은 만료된 청원
-    answered_Petition은 답변된 청원
-    ongoing_Petitiond은 진행중인 청원
-    pending_Petition은 답변 대기중인 청원
-    """
-
-    Petition: List[Petition] = None
-    expiration_Petition: List[Petition] = None
-    answered_Petition: List[Petition] = None
-    ongoing_Petition: List[Petition] = None
-    pending_Petition: List[Petition] = None
 
     class Config:
-        arbitrary_types_allowed = True
+        orm_mode = True
+
+class ListPetitions(Petition):
+
+    end_at : datetime
+    title : str
+    agreed : int
+    status : int
+
+class ViewPetition(ListPetitions):
+
+    contents : str
+    proposal : str
+    created_at : datetime
+    answer : str
