881fc9f73c74fc3af13765972801cfdb05cc301e
sunrabbit123 <qudwls185@naver.com>
2020-12-09 17:39:26 +0900

    MISC dir change(model -> controller)

---
+++ src/controller/petitions.controller.py
@@ -0,0 +1,23 @@
+
+
+def create_petition(data : Schema.CreatePetition):
+    db_petition = Petitions(title = data.title,
+                            contents = data.contents,
+                            proposal = data.proposal,
+                            petitioner = str(data.petitioner))
+    con = db.session
+    con.add(db_petition)
+    con.commit()
+    con.refresh(db_petition)
+    return { "id" : db_petition.petition_id }
+  
+def counting_petition():
+    con = db.session
+
+    total = con.query(Petitions).count()
+    answered = con.query(Petitions).filter(Petitions.status == "answered").count()
+    pending = con.query(Petitions).filter(Petitions.status == "pending").count()
+    
+    return { "total" : total,
+             "answered" : answered,
+             "pending" : pending }
--- src/model/Database/petitions.py
+++ src/model/Database/petitions.py
@@ -47,25 +47,3 @@ class Petitions(Base):
         self.contents = contents
         self.proposal = proposal
         self.petitioner = petitioner
-
-def create_petition(data : Schema.CreatePetition):
-    db_petition = Petitions(title = data.title,
-                            contents = data.contents,
-                            proposal = data.proposal,
-                            petitioner = str(data.petitioner))
-    con = db.session
-    con.add(db_petition)
-    con.commit()
-    con.refresh(db_petition)
-    return { "id" : db_petition.petition_id }
-  
-def counting_petition():
-    con = db.session
-
-    total = con.query(Petitions).count()
-    answered = con.query(Petitions).filter(Petitions.status == "answered").count()
-    pending = con.query(Petitions).filter(Petitions.status == "pending").count()
-    
-    return { "total" : total,
-             "answered" : answered,
-             "pending" : pending }
