57c8cc19e99f89e9cf6f6acdf0a73a7623e52aea
sunrabbit123 <qudwls185@naver.com>
2020-12-16 22:54:32 +0900

    code formatting using black

--- src/controller/student.py
+++ src/controller/student.py
@@ -8,10 +8,18 @@ from controller.auth import auth_account
 def rne(id_token: str):
     data = auth_account(id_token)
     con = db.session
-    check = con.query(Students).filter(Students.std_id == data["sub"])
+    print(data)
+    check = con.query(Students).filter(Students.std_id == data["sub"]).first()
 
     if not check:
-        account = Students(std_id=data["sub"], email=data["email"], name=data["name"])
-        return account
+        account = Students(
+            std_id=data["sub"],
+            email=data["email"],
+            name=data["name"].encode("utf-8"),
+        )
+        con.add(account)
+        con.commit()
+        con.refresh(account)
+        return {"account": account}
 
     return id_token
