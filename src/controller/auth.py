3e47938cb0833c6dc50f045568c2ce096ed5f112
sunrabbit123 <qudwls185@naver.com>
2020-12-16 22:54:46 +0900

    fix not use ()

--- src/controller/auth.py
+++ src/controller/auth.py
@@ -5,4 +5,4 @@ import requests
 def auth_account(id_token: str):
     auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token="
     res = requests.get(f"{auth_url}{id_token}")
-    return res.json
+    return res.json()
