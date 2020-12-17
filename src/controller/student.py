bdb6f044d21e5d8a987446ae57526741a55a712e
sunrabbit123 <qudwls185@naver.com>
2020-12-17 21:05:09 +0900

    update check, return email and name

--- src/controller/student.py
+++ src/controller/student.py
@@ -1,6 +1,7 @@
 import requests
 from fastapi_sqlalchemy import db
 
+from model.Database.petitions import Petitions
 from model.Database.students import Students
 from controller.auth import auth_account
 
@@ -8,18 +9,15 @@ from controller.auth import auth_account
 def rne(id_token: str):
     data = auth_account(id_token)
     con = db.session
-    print(data)
-    check = con.query(Students).filter(Students.std_id == data["sub"]).first()
-
+    try:
+        check = con.query(Students).filter(Students.std_id == data["sub"]).first()
+    except (KeyError):
+        return {"error": "만료된 토큰입니다.", "test": account}
     if not check:
-        account = Students(
-            std_id=data["sub"],
-            email=data["email"],
-            name=data["name"].encode("utf-8"),
-        )
+        account = Students(std_id=data["sub"], email=data["email"], name=data["name"])
         con.add(account)
         con.commit()
         con.refresh(account)
-        return {"account": account}
+        return {"information": {"email": data["email"], "name": data["name"]}}
 
-    return id_token
+    return {"information": {"email": check.email, "name": check.name}}
