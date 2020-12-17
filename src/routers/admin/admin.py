eb31321277d88b0627a13923085c79c2d8cc285c
hmoon630 <s19017@gsm.hs.kr>
2020-12-17 16:18:27 +0900

    Add request body type

--- src/routers/admin/admin.py
+++ src/routers/admin/admin.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
 
+from model.Schema import Answer
+
 admin = APIRouter()
 
 
 @admin.post("/answers/{id}")
-def register_answers(id: int):
+def register_answers(id: int, req_form: Answer):
     # TODO 청원 답변 등록 기능 구현
     return "answers"
 
 
 @admin.put("answers/{id}")
-def modify_answers(id: int):
+def modify_answers(id: int, req_form: Answer):
     # TODO 청원 답변 수정 기능 구현
     return "answers"
