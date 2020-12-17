7ab1fe5135fea0873e435aa5ed55bd3cf3102108
hmoon630 <s19017@gsm.hs.kr>
2020-12-17 20:17:48 +0900

    Add authorization header to admin router

--- src/routers/admin/admin.py
+++ src/routers/admin/admin.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
+from typing import Optional
 
 from model.Schema import Answer
 
@@ -6,12 +7,16 @@ admin = APIRouter()
 
 
 @admin.post("/answers/{id}")
-def register_answers(id: int, req_form: Answer):
+def register_answers(
+    id: int, req_form: Answer, authorization: Optional[str] = Header(None)
+):
     # TODO 청원 답변 등록 기능 구현
     return "answers"
 
 
 @admin.put("answers/{id}")
-def modify_answers(id: int, req_form: Answer):
+def modify_answers(
+    id: int, req_form: Answer, authorization: Optional[str] = Header(None)
+):
     # TODO 청원 답변 수정 기능 구현
     return "answers"
