b648f4dfcabd934f21a2fbff0db0591bdcb60cd6
sunrabbit123 <qudwls185@naver.com>
2020-12-21 16:30:47 +0900

    Update print(error)

--- src/controller/users.py
+++ src/controller/users.py
@@ -15,9 +15,11 @@ def get_user_info(id_token: str):
         return {"error": "만료된 토큰입니다."}
     try:
         return {"information": {"email": check.email, "name": check.name}}
-    except (Exception e):
+    except Exception as e:
+        # 에러는 나올텐데, 무슨에러일지를 몰라서 우선적으로 에러 출력하게 Exception으로 적어놓았어요
+        print(e)
         return None
-        
+
 
 def register_user(id_token: str):
     account = Users(std_id=data["sub"], email=data["email"], name=data["name"])
