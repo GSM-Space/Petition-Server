5535c3b58bf29128fd9d536343ef0776bf018570
정현문 <48788794+hmoon630@users.noreply.github.com>
2020-12-21 16:53:17 +0900

    Update src/controller/petitions.py
    Co-authored-by: 오병진 <64676070+sunrabbit123@users.noreply.github.com>

--- src/controller/petitions.py
+++ src/controller/petitions.py
@@ -78,7 +78,7 @@ class PetitionController:
         return {"total": total, "answered": answered, "pending": pending}
 
     @staticmethod
-    def search_petitions():
+    def search_petitions(q: str, page: int):
         return
 
     @staticmethod
