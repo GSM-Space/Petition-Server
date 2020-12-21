530be66ca31c3571b409636aecfb680001a2ce7e
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2020-12-21 14:13:09 +0900

    Update src/controller/auth.py
    그렇게 변경하는게 더 이해하기 편할 것 같네요 :D
    
    Co-authored-by: 정현문 <48788794+hmoon630@users.noreply.github.com>

--- src/controller/auth.py
+++ src/controller/auth.py
@@ -2,7 +2,7 @@ from fastapi_sqlalchemy import db
 import requests
 
 
-def auth_account(id_token: str):
+def auth_by_token(id_token: str):
     auth_url = "https://oauth2.googleapis.com/tokeninfo?id_token="
     res = requests.get(f"{auth_url}{id_token}")
     return res.json()
