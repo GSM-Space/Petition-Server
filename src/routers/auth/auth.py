da29c064edf9f16bddcca46a797629e45ad60fda
hmoon630 <s19017@gsm.hs.kr>
2021-04-27 14:26:10 +0900

    ADD: return 400 when token is invalid

--- src/routers/auth/auth.py
+++ src/routers/auth/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Request, Response
 from typing import Optional
 
 from controller.users import UserController
@@ -8,7 +8,9 @@ auth = APIRouter()
 
 
 @auth.post("/socialmedia")
-def socialmedia(authorization: Optional[str] = Header(None)):
+def socialmedia(response: Response, authorization: Optional[str] = Header(None)):
     user = UserController(id_token=authorization)
     info = user.get_user_info()
+    if info["error"]:
+        response.status_code = 400
     return info
