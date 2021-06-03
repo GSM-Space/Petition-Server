c5ddd081452834f59ecfd0abf5a34dc2b8a66b61
hmoon630 <s19017@gsm.hs.kr>
2021-06-03 18:03:03 +0900

    refactor: user controller

--- src/controller/users.py
+++ src/controller/users.py
@@ -5,40 +5,23 @@ from typing import List, Optional
 
 from model.Database.petitions import Petitions
 from model.Database.users import Users, Authority
-
-from controller.auth import auth_by_token
+from model.Schema.user import User
 
 
 class UserController:
-    def __init__(
-        self,
-        id: str = None,
-        email: str = None,
-        name: str = None,
-        authority: str = None,
-        my_petitions: Optional[List[Petitions]] = None,
-        agreed: int = None,
-        id_token: str = None,
-    ):
-        self.id = id
-        self.email = email
-        self.name = name
-        self.authority = authority
-        self.my_petitions = my_petitions
-        self.agreed = agreed
-        self.data = auth_by_token(id_token)
+    def __init__(self):
+        pass
 
-    def get_user_info(self):
+    @staticmethod
+    def get_user(user_id: int):
         con = db.session
-        try:
-            check = con.query(Users).filter(Users.std_id == self.data["sub"]).first()
-        except (KeyError):
-            return {"error": "만료된 토큰입니다."}
-        try:
-            self.id = check.std_id
-            return {"information": {"email": check.email, "name": check.name}}
-        except AttributeError:
-            return UserController.register_user(self.data)
+        queried_user = con.query(Users).filter(Users.std_id == user_id).first()
+        user = (
+            User(id=queried_user.std_id, **queried_user.__dict__)
+            if queried_user
+            else None
+        )
+        return user
 
     @staticmethod
     def register_user(data):
@@ -52,11 +35,3 @@ class UserController:
         con.commit()
         con.refresh(account)
         return {"information": {"email": data["email"], "name": data["name"]}}
-
-    def auth_admin(self):
-        con = db.session
-        try:
-            check = con.query(Users).filter(Users.std_id == self.data["sub"]).first()
-            return check.authority
-        except (KeyError):
-            return {"error": "만료된 토큰입니다."}
